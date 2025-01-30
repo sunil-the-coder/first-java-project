95f1b86f136e83d428dfd9d8c3e55368716900b3
sunil-the-coder <sunilpatil10june@gmail.com>
2025-01-30 10:02:07 +0530

    removed add feature

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,7 +1,4 @@
-class Calculator {
-    public int add(int a, int b) {
-        return a + b;
-    }
+class Calculator { 
 
     public int substract(int a, int b) {
         return a - b;
@@ -12,7 +9,7 @@ public class CalculatorTest
 {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        System.out.println("Result:"+ calc.add(10, 20));
+       // System.out.println("Result:"+ calc.add(10, 20));
 
         System.out.println("Result:"+ calc.substract(10, 20));
     }
