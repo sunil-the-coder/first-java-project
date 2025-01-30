1bca78a3fcff1a8316c80ef590046a3f2ff578dd
Sunil Patil <sunilpatil10june@gmail.com>
2025-01-30 10:28:03 +0530

    added multiplication feature

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -7,6 +7,10 @@ class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
 }
 
 public class CalculatorTest
@@ -16,5 +20,6 @@ public class CalculatorTest
        System.out.println("Result:"+ calc.add(10, 20));
 
         System.out.println("Result:"+ calc.substract(10, 20));
+        System.out.println("Result:"+ calc.mult(10, 20));
     }
 }
