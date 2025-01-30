f9ce29059a7667754a8c4c48b358d40110b5dfa0
sarikapawar <sarika.pawar9oct@gmail.com>
2025-01-29 23:36:50 -0500

    add function added

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -3,13 +3,17 @@ class Calculator {
     public int substract(int a, int b) {
         return a - b;
     }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
 
 public class CalculatorTest
 {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-       // System.out.println("Result:"+ calc.add(10, 20));
+       System.out.println("Result:"+ calc.add(10, 20));
 
         System.out.println("Result:"+ calc.substract(10, 20));
     }
